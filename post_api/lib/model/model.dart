f2d79bc6da52038e46a321fd2e0eed44141cbc56
purnima-poo <np03a190173@herladcollege.edu.np>
2022-06-13 17:32:00 +0545

    feat:added model in model class

--- post_api/lib/model/model.dart
+++ post_api/lib/model/model.dart
@@ -1,1 +1,26 @@
-class ProductModel {}
+class ProductModel {
+  final int? id;
+  final String? title;
+  final String? price;
+  final String? description;
+  final String? image;
+  final String? category;
+
+  const ProductModel(
+      {required this.id,
+      required this.title,
+      required this.price,
+      required this.description,
+      required this.image,
+      required this.category});
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+        id: json['id'],
+        title: json['title'],
+        price: json['price'],
+        description: json['description'],
+        image: json['image'],
+        category: json['category']);
+  }
+}
